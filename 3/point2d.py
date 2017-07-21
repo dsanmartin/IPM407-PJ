1bdabe9b5309772b3b2e82e126078ee0a7ea7f69
Daniel San Martin <dsanmartinreyes@gmail.com>
2017-07-21 19:27:46 -0400

    Limpieza codigo

--- 3/point2d.py
+++ 3/point2d.py
@@ -1,7 +1,15 @@
+# Class to manipulate 2D source/target points
 from math import sqrt
 
+
 class point2d():
-	def __init__(self,x,y,gamma=0,idx=-1):
+	def __init__(self, x, y, gamma=0, idx=-1):
+		"""
+		x: x axis
+		y: y axis
+		gamma: field
+		idx: index
+		"""
 		self.x = x
 		self.y = y
 		self.gamma = gamma
@@ -10,7 +18,7 @@ class point2d():
 	def __str__(self):
 		return '(%.2f,%.2f,%.2f,%d)' %(self.x,self.y,self.gamma,self.idx)
 	
-	# arithmetic - only deal with spatial (x,y)
+	# Arithmetic manipulation, only deal with spatial (x,y)
 	def __add__(self,other):
 		new_x = self.x + other.x
 		new_y = self.y + other.y
@@ -23,20 +31,21 @@ class point2d():
 		
 		return point2d(new_x,new_y,self.gamma,self.idx)
 	
-	# multiplication by scalar
+	# Multiplication by scalar
 	def __mul__(self, a):
 		new_x = self.x*a
 		new_y = self.y*a
 		
 		return point2d(new_x,new_y)
 	
-	# division by scalar
+	# Division by scalar
 	def __truediv__(self,b):
 		new_x = self.x / b
 		new_y = self.y / b
 		
 		return point2d(new_x,new_y)
 	
+	# 2D point norm
 	def norm(self):
 		return sqrt(self.x**2 + self.y**2)
 
